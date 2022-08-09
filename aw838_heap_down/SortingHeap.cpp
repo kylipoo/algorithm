36c1bf0f55580d1ae31ee04597448f1d591a1309
Kyle Li <kyleyli2005@gmail.com>
2022-08-09 16:57:46 -0700

    fix bug

--- aw838_heap_down/SortingHeap.cpp
+++ aw838_heap_down/SortingHeap.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 const int N = 1e5+10;
-int h[N], size;
-int n, m;
+int h[N];
+int n, m, arrSize;
 
 void down(int u) {
     int t = u;
-    if(2*u < size && h[2*u] < h[u]) {
+    if(2*u <= arrSize && h[2*u] < h[t]) {
         t = 2*u;
     }
-    if(2*u+1 < size && h[2*u+1] < h[u]) {
+    if(2*u+1 <= arrSize && h[2*u+1] < h[t]) {
         t = 2*u+1;
     }
-    if(t != u) {
+    if(t!=u) {
         swap(h[u], h[t]);
         down(t);
     }
@@ -22,17 +23,14 @@ void down(int u) {
 
 int main() {
     scanf("%d%d", &n, &m);
-    size = n;
-    for(int i=1; i<=n; i++) {
-        cin >> h[i];
-    }
-    for(int i=n/2; i; i--) {
-        down(i);
-    }
+    for(int i=1; i<=n; i++) cin >> h[i];
+    arrSize = n;
+    for(int i=n/2; i; i--) down(i);
     while(m--) {
         printf("%d ", h[1]);
-        h[1] = h[size];
-        size--;
+        h[1] = h[arrSize];
+        arrSize--;
         down(1);
     }
+    return 0;
 }
