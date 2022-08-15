64fda7a65fc771e18af21f891c55dde1ffc0daff
Kyle Li <kyleyli2005@gmail.com>
2022-08-15 15:18:31 -0700

    fix bug

--- aw785_quicksort/QuickSort.cpp
+++ aw785_quicksort/QuickSort.cpp
@@ -11,10 +11,12 @@ void quickSort(int q[], int l, int r) {
         return;
     }
     int x = q[l], i=l-1, j=r+1;
-    do i++; while(q[i] < x);
-    do j--; while(q[j] > x);
-    if(i < j) {
-        swap(q[i], q[j]);
+    while(i<j) {
+        do i++; while (q[i] < x);
+        do j--; while (q[j] > x);
+        if (i < j) {
+            swap(q[i], q[j]);
+        }
     }
     quickSort(q, l, j);
     quickSort(q, j+1, r);
