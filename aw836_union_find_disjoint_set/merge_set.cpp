b6b90de39fa4fd3cb2bf424578d63a6c68aef94f
Kyle Li <kyleyli2005@gmail.com>
2022-09-04 20:32:49 -0700

    more notes

---
+++ aw836_union_find_disjoint_set/merge_set.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1e5+10;
+int p[N];
+
+int find(int x) {
+    if(p[x]!=x) p[x] = find(p[x]);
+    return p[x];
+}
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i=1; i<=n; i++) {
+        p[i] = i;
+    }
+    while(m--) {
+        char op;
+        int a, b;
+        cin >> op >> a >> b;
+        if(op == 'M') {
+            p[find(a)] = find(b);
+        } else {
+            if(find(a) == find(b)) puts("Yes");
+            else puts("No");
+        }
+    }
+    return 0;
+}
